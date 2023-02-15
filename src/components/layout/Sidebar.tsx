0d589c9f9d460afb10c98fe0cf25b50c9583609f
sooyeoniya <tndus1306403@naver.com>
2023-02-15 13:48:31 +0900

    Refactoring sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import {GiChameleonGlyph} from 'react-icons/gi';
-import {MdOutlineCancel} from 'react-icons/md';
 import {links} from '../../assets/Dummy';
 import {useStateContext} from '../../contexts/ContextProvider';
-
+import {GiChameleonGlyph} from 'react-icons/gi';
+import {MdOutlineCancel} from 'react-icons/md';
 export default function Sidebar() {
   const {currentColor, activeMenu, setActiveMenu, handleCloseSideBar} = useStateContext();
 
