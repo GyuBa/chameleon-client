4d0d8c0f13da5fbb65fbbe5b0694d1b81fc586a1
refracta <58779799+refracta@users.noreply.github.com>
2023-03-28 18:22:14 +0900

    Add USE_LOCAL flag

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const backendPaths = ['/api'];
+const useLocal = process.env.USE_LOCAL?.toLowerCase() === 'true' || false;
+console.log('setupProxy.js useLocal:', useLocal);
 
 module.exports = function (app) {
     backendPaths.forEach(path => {
         app.use(
             createProxyMiddleware(path, {
-                target: 'https://dev-platform.chameleon.best',
+                target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
@@ -15,7 +17,7 @@ module.exports = function (app) {
     });
     app.use(
         createProxyMiddleware('/websocket', {
-            target: 'https://dev-platform.chameleon.best',
+            target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
             changeOrigin: true,
             ws: true,
             secure: false
