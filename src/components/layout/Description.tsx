0178685893a9f11daaaf9d886d0b1fc89e3925fd
sooyeoniya <tndus1306403@naver.com>
2023-04-03 16:28:39 +0900

    Implement model list & description API

--- src/components/layout/Description.tsx
+++ src/components/layout/Description.tsx
@@ -5,13 +5,29 @@ import MDEditor from "@uiw/react-md-editor";
 import {Button} from "../index";
 import {useStateContext} from "../../contexts/ContextProvider";
 
-export default function Description() {
+interface DescriptionProps {
+    descriptionInfo: {
+        username: string;
+        modelname: string;
+        region: string;
+        input: string;
+        output: string;
+        description: string;
+    };
+}
+
+export default function Description({descriptionInfo}: DescriptionProps) {
     const {currentColor, value} = useStateContext();
+    const {modelname, username, region, input, output, description} = descriptionInfo;
+
+    if (!descriptionInfo) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className="contents">
             <div className="m-2 md:my-10 mt-24 p-2 md:pr-5 md:py-10 bg-white rounded-3xl overflow-auto">
                 <div className="flex justify-between items-center pb-6 border-b-1 border-gray-300 overflow-auto overflow-scroll max-h-screen">
-                    <p className="text-3xl font-extrabold tracking-tight text-slate-900">Model01</p>
+                    <p className="text-3xl font-extrabold tracking-tight text-slate-900">{modelname}</p>
                     <div className="flex gap-2">
                         <Link to="/model/execute" state={{}}>
                             <Button style={{backgroundColor: currentColor, color: "white", borderRadius: "10px"}}
@@ -20,22 +36,21 @@ export default function Description() {
                     </div>
                 </div>
                 <div className="mt-8 overflow-auto overflow-scroll max-h-screen">
-                    {/*임시 데이터*/}
                     <div className="flex my-2 items-center">
                         <p className="text-lg font-bold">Model Name:ㅤ</p>
-                        <p>Model01</p>
+                        <p>{modelname}</p>
                     </div>
                     <div className="flex my-2 items-center">
                         <p className="text-lg font-bold">Model Developer:ㅤ</p>
-                        <p>최수연</p>
+                        <p>{username}</p>
                     </div>
                     <div className="flex my-2 items-center">
                         <p className="text-lg font-bold">Region:ㅤ</p>
-                        <p>Mongle</p>
+                        <p>{region}</p>
                     </div>
                     <div className="flex">
-                        <div className="py-3"><Badge color="indigo">Input: text</Badge></div>
-                        <div className="p-3"><Badge color="purple">Output: binary</Badge></div>
+                        <div className="py-3"><Badge color="indigo">Input: {input}</Badge></div>
+                        <div className="p-3"><Badge color="purple">Output: {output}</Badge></div>
                     </div>
                     <div className="flex my-2 items-center">
                         <p className="text-lg font-bold">Parameter:ㅤ</p>
@@ -44,6 +59,7 @@ export default function Description() {
                     <div className="my-2 whitespace-pre-wrap">
                         <p className="text-lg font-bold">Model Description:ㅤ</p>
                         <MDEditor.Markdown className="py-5" source={value} style={{whiteSpace: 'pre-wrap'}}/>
+                        {/*{description}*/}
                     </div>
                 </div>
             </div>
--- src/pages/model/board/Model.tsx
+++ src/pages/model/board/Model.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Badge, Table} from "flowbite-react";
 import {myModel} from "../../../assets/Dummy";
 import {Link} from "react-router-dom";
@@ -10,145 +10,184 @@ import {FiList} from "react-icons/fi";
 import {RiDeleteBinLine} from "react-icons/ri";
 import {BiPencil, BiAddToQueue, BiDotsVerticalRounded, BiTrash} from "react-icons/bi";
 import {VscDebugStart} from "react-icons/vsc";
+import instance from "../../../ConstantValue";
+
+interface ModelInfo {
+  username: string;
+  modelname: string;
+  region: string;
+  input: string;
+  output: string;
+  description: string;
+}
 
 export default function Model() {
-    const {
-        menuState,
-        onClickMenu,
-        selectModel,
-        modelState,
-        currentLayout,
-        setCurrentLayout,
-        isDesktopOrMobile
-    } = useStateContext();
+  const {
+    menuState,
+    onClickMenu,
+    modelState,
+    currentLayout,
+    setCurrentLayout,
+    isDesktopOrMobile
+  } = useStateContext();
+  const [modelList, setModelList] = useState<ModelInfo[]>([]);
+  const [selectedModel, setSelectedModel] = useState<ModelInfo | null>(null);
 
-    const ArrangeMenu = () => (
-        <div className="flex items-center gap-2">
-            <Link to="/model/create" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
-                <BiAddToQueue size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델생성</span>
-            </Link>
-            <Link to="/" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
-                <BiPencil size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델수정</span>
-            </Link>
-            <Link to="/" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
-                <BiTrash size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델삭제</span>
-            </Link>
+  useEffect(() => {
+    let completed = false;
+
+    (async function get() {
+      try {
+        const response = await instance.get(`/model`, {
+          timeout: 5000,
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        if (!completed) {
+          setModelList(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    return () => {
+      completed = true;
+    };
+  }, []);
+
+  const onModelSelect = (modelInfo: ModelInfo) => {
+    setSelectedModel(modelInfo);
+  };
+
+
+  const ArrangeMenu = () => (
+    <div className="flex items-center gap-2">
+      <Link to="/model/create" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
+        <BiAddToQueue size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델생성</span>
+      </Link>
+      <Link to="/" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
+        <BiPencil size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델수정</span>
+      </Link>
+      <Link to="/" className="flex items-center rounded-full p-1 hover:bg-light-gray focus:bg-gray">
+        <BiTrash size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델삭제</span>
+      </Link>
+    </div>
+  );
+
+  const DropdownMenu = () => (
+    <div>
+      <button type="button" onClick={onClickMenu}
+              className="relative text-xl rounded-full p-1 hover:bg-light-gray focus:bg-gray">
+        {<BiDotsVerticalRounded aria-hidden="true" size="30"/>}
+      </button>
+      {menuState ? <Menu/> : null}
+    </div>
+  );
+
+  const Menu = () => (
+    <div className="nav-item absolute right-4 top-30 bg-white drop-shadow-lg py-2 px-4 rounded-lg w-36">
+      <Link to="/model/create" className="flex gap-1 border-b-1 border-gray-400 hover:bg-gray-100 items-center">
+        <BiAddToQueue size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델생성</span>
+      </Link>
+      <Link to="/" className="flex gap-1 border-b-1 border-gray-400 hover:bg-gray-100 items-center">
+        <BiPencil size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델수정</span>
+      </Link>
+      <Link to="/" className="flex gap-1 hover:bg-gray-100 items-center">
+        <RiDeleteBinLine size="25" color="#484848" className="pl-1"/>
+        <span
+          className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델삭제</span>
+      </Link>
+    </div>
+  );
+
+  const GridLayout = () => (
+    <div
+      className="grid xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-2 gap-4 mt-10 overflow-auto overflow-scroll max-h-screen">
+      {modelList.map((modelInfo, index) => (
+        <div key={index} onClick={() => onModelSelect(modelInfo)}
+             className="w-auto px-5 p-5 bg-white rounded-xl drop-shadow-lg hover:drop-shadow-xl cursor-pointer">
+          <p className="border-b-2 font-semibold text-xl break-all">{modelInfo?.modelname}</p>
+          <div className="flex">
+            <div className="py-3"><Badge color="indigo">Input: {modelInfo?.input}</Badge></div>
+            <div className="p-3"><Badge color="purple">Output: {modelInfo?.output}</Badge></div>
+          </div>
+          <div className="flex mt-10 justify-between">
+            <div className="text-sm text-gray-500 py-3">Updated 2022.03.01. · {modelInfo?.username} · 20KB</div>
+            <div className="py-3"><Badge color="gray">{modelInfo?.region}</Badge></div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 
-    const DropdownMenu = () => (
-        <div>
-            <button type="button" onClick={onClickMenu}
-                    className="relative text-xl rounded-full p-1 hover:bg-light-gray focus:bg-gray">
-                {<BiDotsVerticalRounded aria-hidden="true" size="30"/>}
+  const ListLayout = () => (
+    <div className="mt-10 overflow-auto overflow-scroll max-h-screen">
+      <Table hoverable={true}>
+        <Table.Head>
+          {myModel.header.map((item) => (
+            <Table.HeadCell>{item}</Table.HeadCell>
+          ))}
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {modelList.map((modelInfo) => (
+            <Table.Row className="bg-white">
+              <Table.Cell
+                className="whitespace-nowrap font-medium text-gray-900">{modelInfo?.modelname}</Table.Cell>
+              <Table.Cell>
+                <div className="flex"><Badge color="indigo">{modelInfo?.input}</Badge></div>
+              </Table.Cell>
+              <Table.Cell>
+                <div className="flex"><Badge color="purple">{modelInfo?.output}</Badge></div>
+              </Table.Cell>
+              <Table.Cell>{modelInfo?.username}</Table.Cell>
+              <Table.Cell>
+                <VscDebugStart onClick={() => onModelSelect(modelInfo)}
+                               className="text-white py-1 w-10 h-6 rounded bg-blue-500 hover:bg-blue-600 hover:drop-shadow-lg"/>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+
+  return (
+    <div className="contents">
+      <div className="w-full m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl overflow-auto">
+        <div className="flex justify-between items-center">
+          <div className="flex">
+            <Header category="" title="Models"/>
+            <button onClick={() => setCurrentLayout("GridLayout")} type="button"
+                    className={`ml-2 mr-1 text-xl rounded-full p-2 hover:bg-light-gray focus:bg-gray ${currentLayout === "GridLayout" ? "bg-light-gray" : null}`}>
+              {<HiViewGrid size="21" className="text-gray-500"/>}
             </button>
-            {menuState ? <Menu/> : null}
+            <button onClick={() => setCurrentLayout("ListLayout")} type="button"
+                    className={`text-xl rounded-full p-2 hover:bg-light-gray focus:bg-gray ${currentLayout === "ListLayout" ? "bg-light-gray" : null}`}>
+              {<FiList size="21" className="text-gray-500"/>}
+            </button>
+          </div>
+          {!isDesktopOrMobile ? <ArrangeMenu/> : <DropdownMenu/>}
         </div>
-    );
-
-    const Menu = () => (
-        <div className="nav-item absolute right-4 top-30 bg-white drop-shadow-lg py-2 px-4 rounded-lg w-36">
-            <Link to="/model/create" className="flex gap-1 border-b-1 border-gray-400 hover:bg-gray-100 items-center">
-                <BiAddToQueue size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델생성</span>
-            </Link>
-            <Link to="/" className="flex gap-1 border-b-1 border-gray-400 hover:bg-gray-100 items-center">
-                <BiPencil size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델수정</span>
-            </Link>
-            <Link to="/" className="flex gap-1 hover:bg-gray-100 items-center">
-                <RiDeleteBinLine size="25" color="#484848" className="pl-1"/>
-                <span
-                    className="text-gray-700 flex justify-between w-full px-1 py-2 text-sm leading-5 text-left">모델삭제</span>
-            </Link>
-        </div>
-    );
-
-    const GridLayout = () => (
-        <div
-            className="grid xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-2 gap-4 mt-10 overflow-auto overflow-scroll max-h-screen">
-            {myModel.data.map((item) => (
-                <div onClick={selectModel}
-                     className="w-auto px-5 p-5 bg-white rounded-xl drop-shadow-lg hover:drop-shadow-xl cursor-pointer">
-                    <p className="border-b-2 font-semibold text-xl break-all">{item.name}</p>
-                    <div className="flex">
-                        <div className="py-3"><Badge color="indigo">Input: text</Badge></div>
-                        <div className="p-3"><Badge color="purple">Output: binary</Badge></div>
-                    </div>
-                    <div className="flex mt-10 justify-between">
-                        <div className="text-sm text-gray-500 py-3">Updated 2022.03.01. · 최수연 · 20KB</div>
-                        <div className="py-3"><Badge color="gray">Mongle</Badge></div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-
-    const ListLayout = () => (
-        <div className="mt-10 overflow-auto overflow-scroll max-h-screen">
-            <Table hoverable={true}>
-                <Table.Head>
-                    {myModel.header.map((item) => (
-                        <Table.HeadCell>{item}</Table.HeadCell>
-                    ))}
-                </Table.Head>
-                <Table.Body className="divide-y">
-                    {myModel.data.map((item) => (
-                        <Table.Row className="bg-white">
-                            <Table.Cell
-                                className="whitespace-nowrap font-medium text-gray-900">{item.name}</Table.Cell>
-                            <Table.Cell>
-                                <div className="flex"><Badge color="indigo">{item.input}</Badge></div>
-                            </Table.Cell>
-                            <Table.Cell>
-                                <div className="flex"><Badge color="purple">{item.output}</Badge></div>
-                            </Table.Cell>
-                            <Table.Cell>{item.developer}</Table.Cell>
-                            <Table.Cell>{item.date}</Table.Cell>
-                            <Table.Cell>{item.size}</Table.Cell>
-                            <Table.Cell>
-                                <VscDebugStart onClick={selectModel}
-                                               className="text-white py-1 w-10 h-6 rounded bg-blue-500 hover:bg-blue-600 hover:drop-shadow-lg"/>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
-        </div>
-    );
-
-    return (
-        <div className="contents">
-            <div className="w-full m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl overflow-auto">
-                <div className="flex justify-between items-center">
-                    <div className="flex">
-                        <Header category="" title="Models"/>
-                        <button onClick={() => setCurrentLayout("GridLayout")} type="button"
-                                className={`ml-2 mr-1 text-xl rounded-full p-2 hover:bg-light-gray focus:bg-gray ${currentLayout === "GridLayout" ? "bg-light-gray" : null}`}>
-                            {<HiViewGrid size="21" className="text-gray-500"/>}
-                        </button>
-                        <button onClick={() => setCurrentLayout("ListLayout")} type="button"
-                                className={`text-xl rounded-full p-2 hover:bg-light-gray focus:bg-gray ${currentLayout === "ListLayout" ? "bg-light-gray" : null}`}>
-                            {<FiList size="21" className="text-gray-500"/>}
-                        </button>
-                    </div>
-                    {!isDesktopOrMobile ? <ArrangeMenu/> : <DropdownMenu/>}
-                </div>
-                {currentLayout === "GridLayout" ? <GridLayout/> : <ListLayout/>}
-            </div>
-            {modelState ?
-                <div className="w-2/6 ease-in-out duration-300 translate-x-0"><Description/></div>
-                :
-                <div className="w-0 ease-in-out duration-300 translate-x-full"><Description/></div>
-            }
-        </div>
-    );
+        {currentLayout === "GridLayout" ? <GridLayout/> : <ListLayout/>}
+      </div>
+      {selectedModel && modelState ?
+        <div className="w-2/6 ease-in-out duration-300 translate-x-0"><Description descriptionInfo={selectedModel}/></div>
+        :
+        <div className="w-0 ease-in-out duration-300 translate-x-full">{/*<Description descriptionInfo={undefined}/>*/}</div>
+      }
+    </div>
+  );
 };
