42461814517a3f6329dfe1f1d5005adb24a49645
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2023-04-14 02:02:24 +0900

    Refactor WebSocket Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route, useLocation, useMatch} from 'react-router-dom';
 import {Layout} from './components';
 import './App.css';
 import './styles/Dropzone.css';
@@ -23,38 +23,40 @@ import useWebSocket from "react-use-websocket";
 
 export default function App() {
 
+    const location = useLocation();
+
     const {
-        sendMessage,
+        sendJsonMessage,
     } = useWebSocket((window.location.protocol.startsWith('https') ? 'wss://' : 'ws://') + window.location.host + '/websocket', {
         shouldReconnect: (closeEvent) => true,
     });
 
     useEffect(() => {
-        // 페이지가 바뀔 때마다 메시지를 보냅니다.
-        sendMessage(JSON.stringify({ msg: 'path', path: window.location.pathname }));
-    }, [sendMessage, window.location.pathname]);
+        sendJsonMessage({ msg: 'path', path: window.location.pathname });
+    }, [sendJsonMessage, window.location.pathname]);
+
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={(<Layout/>)}>
-                    <Route path="/main" element={(<Main/>)}/>
-                    <Route path="/account" element={<Account/>}/>
-                    <Route path="/change-password" element={<ChangePassword/>}/>
-                    <Route path="/payment" element={<Payment/>}/>
-                    <Route path="/ws-test" element={<WebSocket/>}/>
-                    {/*모델별 경로 고려*/}
-                    <Route path="/model" element={<Model/>}/>
-                    <Route path="/model/execute" element={<ExecuteModel/>}/>
-                    <Route path="/model/create" element={<CreateModel/>}/>
-                    <Route path="/model/create/description" element={<CreateDescription/>}/>
-                    <Route path="/model/create/parameter" element={<CreateParameter/>}/>
-                    <Route path="/menu/tar-file" element={<TarFile/>}/>
-                    <Route path="/menu/dockerfile" element={<Dockerfile/>}/>
-                </Route>
-                <Route path="/sign-in" element={(<SignIn/>)}/>
-                <Route path="/sign-up" element={(<SignUp/>)}/>
-            </Routes>
-        </BrowserRouter>
+
+        <Routes>
+            <Route path="/" element={(<Layout/>)}>
+                <Route path="/main" element={(<Main/>)}/>
+                <Route path="/account" element={<Account/>}/>
+                <Route path="/change-password" element={<ChangePassword/>}/>
+                <Route path="/payment" element={<Payment/>}/>
+                <Route path="/ws-test" element={<WebSocket/>}/>
+                {/*모델별 경로 고려*/}
+                <Route path="/model" element={<Model/>}/>
+                <Route path="/model/execute" element={<ExecuteModel/>}/>
+                <Route path="/model/create" element={<CreateModel/>}/>
+                <Route path="/model/create/description" element={<CreateDescription/>}/>
+                <Route path="/model/create/parameter" element={<CreateParameter/>}/>
+                <Route path="/menu/tar-file" element={<TarFile/>}/>
+                <Route path="/menu/dockerfile" element={<Dockerfile/>}/>
+            </Route>
+            <Route path="/sign-in" element={(<SignIn/>)}/>
+            <Route path="/sign-up" element={(<SignUp/>)}/>
+        </Routes>
+
     );
 };
