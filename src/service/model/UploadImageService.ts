ba92c905f388e22793b099984512fcee289f3c0f
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-04-27 02:06:02 +0900

    Implement model creation loading screen & Delete unused file

--- src/service/model/UploadImageService.ts
+++
@@ -1,6 +0,0 @@
-import React, {useState} from "react";
-import instance from "../../ConstantValue";
-
-export default function useUploadImageInfo(){
- // TODO: CreateModelTab, CreateDescription 의 post 요청 통합
-}
