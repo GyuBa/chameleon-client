d4dcbe0ac0abb21301499f95208bdf60852d87aa
refracta <58779799+refracta@users.noreply.github.com>
2023-05-04 12:35:19 +0900

    Add enums

--- src/types/chameleon-client.enum.ts
+++ src/types/chameleon-client.enum.ts
@@ -1,10 +1,37 @@
+export enum HistoryStatus {
+    CACHED = 'cached',
+    INITIALIZING = 'initializing',
+    RUNNING = 'running',
+    ERROR = 'error',
+    FINISHED = 'finished',
+}
+
 export enum WSMessageType {
-    Ready = 'Ready',
-    Path = 'Path',
-    TerminalResize = 'TerminalResize',
-    Terminal = 'Terminal',
-    UpdateModel = 'UpdateModel',
-    UpdateModels = 'UpdateModels',
-    UpdateHistory = 'UpdateHistory',
-    UpdateHistories = 'UpdateHistories'
+    READY = 'Ready',
+    PATH = 'Path',
+    TERMINAL_RESIZE = 'TerminalResize',
+    TERMINAL = 'Terminal',
+    UPDATE_MODEL = 'UpdateModel',
+    UPDATE_MODELS = 'UpdateModels',
+    UPDATE_HISTORY = 'UpdateHistory',
+    UPDATE_HISTORIES = 'UpdateHistories'
+}
+
+export enum SocketMessageType {
+    HELLO = 'Hello',
+    LAUNCH = 'Launch',
+    FILE_WAIT = 'FileWait',
+    FILE_RECEIVE_END = 'FileReceiveEnd',
+    TERMINAL = 'Terminal',
+    TERMINAL_RESIZE = 'TerminalResize',
+    PROCESS_END = 'ProcessEnd',
+    FILE = 'File',
+    REQUEST_FILE = 'RequestFile',
+    WAIT_RECEIVE = 'WaitReceive',
+    LAUNCH_MODEL = 'LaunchModel'
+}
+
+export enum SocketReceiveMode {
+    JSON,
+    FILE
 }
