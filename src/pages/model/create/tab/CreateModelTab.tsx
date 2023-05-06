5d92a0d6ebfed8250a2428233ad4f384a4cde8eb
Gyuba <sansam1010@naver.com>
2023-05-07 02:32:54 +0900

    fix: modify again `Fix defaultValue related warning: 11f9b9d3`

--- src/pages/model/create/tab/CreateModelTab.tsx
+++ src/pages/model/create/tab/CreateModelTab.tsx
@@ -52,7 +52,6 @@ export default function CreateModelTab(number: number) {
             try {
                 const regions = await PlatformAPI.getRegionList();
                 if (!completed) {
-                    console.log(regions);
                     setRegionList(regions);
                 }
             } catch (error) {
@@ -128,7 +127,7 @@ export default function CreateModelTab(number: number) {
                   focus:border-blue-600 focus:outline-none"
                                 value={inputType}
                                 onChange={handleInputTypeChange}>
-                            <option disabled hidden>Input Type</option>
+                            <option value="" disabled hidden>Input Type</option>
                             <option value="none">(none)</option>
                             <option value="image">image</option>
                             <option value="binary">binary</option>
@@ -144,7 +143,7 @@ export default function CreateModelTab(number: number) {
                   focus:border-blue-600 focus:outline-none"
                                 value={outputType}
                                 onChange={handleOutputTypeChange}>
-                            <option disabled hidden>Output Type</option>
+                            <option value="" disabled hidden>Output Type</option>
                             <option value="image">image</option>
                             <option value="binary">binary</option>
                             <option value="text">text</option>
@@ -161,7 +160,7 @@ export default function CreateModelTab(number: number) {
                   focus:border-blue-600 focus:outline-none"
                                 value={regionName}
                                 onChange={handleRegionNameChange}>
-                            <option disabled hidden>Model Region</option>
+                            <option value="" disabled hidden>Model Region</option>
                             {regionList.map((region: { id: number; name: string; }) => (
                                 <option key={region.id} value={region.name}>{region.name}</option>
                             ))}
