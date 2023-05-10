1e64f6feb11af8134b7ceed470f63a4685c318b7
Gyuba <sansam1010@naver.com>
2023-05-10 13:05:45 +0900

    feat: basic history list

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -2,11 +2,32 @@ import React, {useEffect, useState} from "react";
 import {PlatformAPI} from "../../platform/PlatformAPI";
 import Header from "../../components/layout/Header";
 import {HistoryList} from "./HistoryList";
-import {HistoryEntityData} from "../../types/chameleon-client.entitydata";
+import {HistoryEntityData} from "../../types/chameleon-platform.common";
 
-const DUMMY_HISTORIES:HistoryEntityData[] = [
-
-];
+const Dummy = [
+    {
+        "id": 197,
+        "createdTime": "2023-05-09T14:35:42.311Z",
+        "updatedTime": "2023-05-09T14:35:43.000Z",
+        "status": "finished",
+        "inputPath": "uploads\\inputs\\2e73dccbc4cef6ce93240780a5943b0a",
+        "inputInfo": {
+            "size": 84954,
+            "mimeType": "image/png",
+            "originalName": "image.png"
+        },
+        "outputPath": "uploads/outputs/1d4d5593605f69355af881620a9985c3",
+        "outputInfo": {
+            "fileName": "output_image.png}",
+            "fileSize": 84954
+        },
+        "startedTime": "2023-05-09T14:35:42.000Z",
+        "parameters": {
+            "param1": "example1",
+            "param2": 2
+        }
+    }
+]
 export default function History() {
     const [histories, setHistories] = useState<HistoryEntityData[]>([]);
 
@@ -15,7 +36,14 @@ export default function History() {
 
         (async function () {
             try {
-                setHistories(DUMMY_HISTORIES);
+                // const histories = await PlatformAPI.getMyHistories();
+                const histories = await PlatformAPI.getHistories();
+                if (!completed) {
+                    console.log('history');
+                    console.log(histories);
+                    // @ts-ignore
+                    setHistories(Dummy);
+                }
             } catch (error) {
                 console.error(error);
             }
--- src/pages/history/HistoryList.tsx
+++ src/pages/history/HistoryList.tsx
@@ -1,11 +1,22 @@
 import {Table} from "flowbite-react";
 import {historyColumn} from "../../assets/Dummy";
 import React from "react";
-import {HistoryEntityData} from "../../types/chameleon-client.entitydata";
+import {HistoryEntityData} from "../../types/chameleon-platform.common";
+import {DateUtils} from "../../utils/DateUtils";
 
 type HistoryRow = {
     rows: HistoryEntityData[]
 }
+
+function getStatusColor(status: string){
+    if (status == 'cached') return '#ffff33'
+    if (status == 'initializing') return '#ffffff'
+    if (status == 'running') return '#00cc66'
+    if (status == 'error') return '#ff6666'
+    if (status == 'finished') return '#3333ff'
+    else return '#ff0000'
+}
+
 export function HistoryList({rows}: HistoryRow) {
     return (
         <div>
@@ -16,13 +27,16 @@ export function HistoryList({rows}: HistoryRow) {
                     ))}
                 </Table.Head>
                 <Table.Body className="divide-y">
-                        <Table.Row className="bg-white">
-                            {
-                                rows.map((row) => (
-                                    <Table.Cell></Table.Cell>
-                                ))
-                            }
+                    {rows.map((row) => (
+                        <Table.Row key={`history-${row.id}`} className={'bg-white'}>
+                            <Table.Cell>{row.id}</Table.Cell>
+                            <Table.Cell>{'name'}</Table.Cell>
+                            <Table.Cell>{'data'}</Table.Cell>
+                            <Table.Cell>{'date'}</Table.Cell>
+                            <Table.Cell>{'executor'}</Table.Cell>
+                            <Table.Cell>{row.status}</Table.Cell>
                         </Table.Row>
+                    ))}
                 </Table.Body>
             </Table>
         </div>
