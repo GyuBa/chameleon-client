4fb274ed104fe34d7ae5f0fbb919d98d768bba03
refracta <58779799+refracta@users.noreply.github.com>
2023-05-17 09:28:43 +0900

    Add parent, numberOfParents to HistoryEntityData

--- src/types/chameleon-platform.common.ts
+++ src/types/chameleon-platform.common.ts
@@ -1,5 +1,4 @@
 /* EntityData & Keys */
-
 export interface HistoryEntityData {
     id: number;
     createdTime: Date;
@@ -13,6 +12,8 @@ export interface HistoryEntityData {
     outputType: ModelOutputType;
     description: string;
     executor: UserEntityData;
+    parent: HistoryEntityData;
+    numberOfParents: number;
     model: ModelEntityData;
     startedTime: Date;
     endedTime: Date;
@@ -33,6 +34,8 @@ export const History: Array<keyof HistoryEntityData> = [
     'outputType',
     'description',
     'executor',
+    'parent',
+    'numberOfParents',
     'model',
     'startedTime',
     'endedTime',
@@ -72,6 +75,8 @@ export interface ModelEntityData {
     outputType: ModelOutputType;
     parameters: ModelParameters;
     config: ModelConfig;
+    category: string;
+    point: number;
 }
 
 export const Model: Array<keyof ModelEntityData> = [
@@ -88,6 +93,8 @@ export const Model: Array<keyof ModelEntityData> = [
     'outputType',
     'parameters',
     'config',
+    'category',
+    'point'
 ];
 
 
@@ -111,12 +118,14 @@ export interface UserEntityData {
     id: number;
     email: string;
     username: string;
+    point: number;
 }
 
 export const User: Array<keyof UserEntityData> = [
     'id',
     'email',
     'username',
+    'point'
 ];
 
 export interface WalletEntityData {
@@ -173,8 +182,30 @@ export enum WSMessageType {
     UPDATE_HISTORIES = 'UpdateHistories'
 }
 
+export type WSPathMessage = {
+    msg: WSMessageType.PATH;
+    path: string;
+};
+
+export type WSTerminalResizeMessage = {
+    msg: WSMessageType.TERMINAL_RESIZE;
+    historyId: number;
+    rows: number;
+    cols: number;
+};
+
+export type WSUpdateHistoryMessage = {
+    msg: WSMessageType.UPDATE_HISTORY;
+    history: HistoryEntityData
+}
+
+export type WSTerminalMessage = {
+    msg: WSMessageType.TERMINAL;
+    data: string;
+}
+
+
 export enum SocketMessageType {
-    HELLO = 'Hello',
     LAUNCH = 'Launch',
     FILE_WAIT = 'FileWait',
     FILE_RECEIVE_END = 'FileReceiveEnd',
@@ -184,8 +215,50 @@ export enum SocketMessageType {
     FILE = 'File',
     REQUEST_FILE = 'RequestFile',
     WAIT_RECEIVE = 'WaitReceive',
-    LAUNCH_MODEL = 'LaunchModel'
+    LAUNCH_MODEL = 'LaunchModel',
+    EXIT = 'Exit'
 }
+
+export type SocketLaunchMessage = {
+    msg: SocketMessageType.LAUNCH;
+    isMainConnection: boolean;
+    historyId: number;
+    executionData?: ExecutionData
+};
+export type SocketFileWaitMessage = { msg: SocketMessageType.FILE_WAIT; };
+export type SocketFileReceiveEndMessage = { msg: SocketMessageType.FILE_RECEIVE_END; };
+export type SocketTerminalMessage = {
+    msg: SocketMessageType.TERMINAL;
+    data: string
+};
+export type SocketTerminalResizeMessage = {
+    msg: SocketMessageType.TERMINAL_RESIZE;
+    rows: number;
+    cols: number;
+};
+export type SocketProcessEndMessage = { msg: SocketMessageType.PROCESS_END; };
+export type SocketFileMessage = {
+    msg: SocketMessageType.FILE;
+    fileSize: number;
+    filePath?: string;
+};
+
+export type SocketRequestFileMessage = {
+    msg: SocketMessageType.REQUEST_FILE,
+    filePath: string;
+};
+
+export type SocketWaitReceiveMessage = { msg: SocketMessageType.WAIT_RECEIVE; };
+export type SocketLaunchModelMessage = {
+    msg: SocketMessageType.LAUNCH_MODEL,
+    scriptPath: string;
+    options: LaunchOptions;
+};
+export type SocketExitMessage = {
+    msg: SocketMessageType.EXIT,
+    code: number;
+    message?: string;
+};
 
 export enum SocketReceiveMode {
     JSON,
@@ -237,7 +310,7 @@ export type ResponseData = {
 }
 
 export type ModelInputInfo = {
-    mimeType: string;
+    mimeType?: string;
     fileSize: number;
     fileName: string;
 }
@@ -269,4 +342,17 @@ export type ModelConfig = {
 export type TerminalResizeOption = {
     rows: number;
     cols: number;
+}
+
+export type LaunchOptions = {
+    rows?: number;
+    cols?: number;
+}
+
+export type ExecutionData = {
+    username: string;
+    uniqueName: string;
+    inputPath?: string;
+    parametersPath?: string;
+    outputPath?: string;
 }
