94a322c7413ee6b093240a6a444db3a3c4af876f
refracta <58779799+refracta@users.noreply.github.com>
2023-05-17 11:21:29 +0900

    Minor fix

--- src/service/authentication/UserInfoService.ts
+++ src/service/authentication/UserInfoService.ts
@@ -2,45 +2,45 @@ import {useEffect, useMemo, useState} from "react";
 import {PlatformAPI} from "../../platform/PlatformAPI";
 import {UserEntityData} from "../../types/chameleon-platform.common";
 
-const USER_INFO_KEY = "user_info";
-const emptyUser: UserEntityData = {id: -1, username: "", email: ""};
-export default function useGetUserInfo(): UserEntityData {
-    const [user, setUser] = useState<UserEntityData>(emptyUser);
+const emptyUser: UserEntityData = {id: -1, username: "", email: "", point: -1};
+let loadedUser: UserEntityData = emptyUser;
 
-    useEffect(() => {
-      let completed = false;
-
-      async function get() {
-        try {
-          if (!completed) {
-            setUser(await PlatformAPI.getLoginUser());
-            localStorage.setItem(USER_INFO_KEY, JSON.stringify(await PlatformAPI.getLoginUser()));
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-      const cachedUserInfo = localStorage.getItem(USER_INFO_KEY);
-
-      if (cachedUserInfo) {
-        setUser(JSON.parse(cachedUserInfo));
-      } else get();
-
-      return () => {
-        completed = true;
-      };
+export default function useGetUserInfo(): { handleSignOut: () => Promise<void> } & {
+    getCookieValue: (name: string) => (string | null),
+    user: UserEntityData
+} {
+    const getCookieValue = useMemo(() => {
+        return (name: string) => {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                const [cookieName, cookieValue] = cookie.split('=');
+                if (cookieName === name) return cookieValue;
+            }
+            return null;
+        };
     }, []);
 
-    const cachedValues = useMemo(() => {
-     return { id: user?.id, username: user?.username, email: user?.email };
-    }, [user]);
+    useEffect(() => {
+        (async () => {
+            if (loadedUser === emptyUser) {
+                try {
+                    loadedUser = await PlatformAPI.getLoginUser();
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        })();
+    }, []);
 
-    // TODO: 로그아웃 구현
-    // ESLint에 나타나지 않게 주석 처리
-    /* const handleSignOut = () => {
-      localStorage.removeItem(USER_INFO_KEY);
-    }; */
+    const handleSignOut = async () => {
+        try {
+            await PlatformAPI.signOut();
+            loadedUser = emptyUser;
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-    return {...cachedValues};
+    return {handleSignOut, getCookieValue, user: loadedUser};
 }
