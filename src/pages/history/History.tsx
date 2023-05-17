8faa78ccf121e41d1a5453f95c8ca2838a908fc5
Gyuba <sansam1010@naver.com>
2023-05-17 13:26:52 +0900

    feat: improve HistoryList

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -4,30 +4,6 @@ import Header from "../../components/layout/Header";
 import {HistoryList} from "./HistoryList";
 import {HistoryEntityData} from "../../types/chameleon-platform.common";
 
-const Dummy = [
-    {
-        "id": 197,
-        "createdTime": "2023-05-09T14:35:42.311Z",
-        "updatedTime": "2023-05-09T14:35:43.000Z",
-        "status": "running",
-        "inputPath": "uploads\\inputs\\2e73dccbc4cef6ce93240780a5943b0a",
-        "inputInfo": {
-            "size": 84954,
-            "mimeType": "image/png",
-            "originalName": "image.png"
-        },
-        "outputPath": "uploads/outputs/1d4d5593605f69355af881620a9985c3",
-        "outputInfo": {
-            "fileName": "output_image.png}",
-            "fileSize": 84954
-        },
-        "startedTime": "2023-05-09T14:35:42.000Z",
-        "parameters": {
-            "param1": "example1",
-            "param2": 2
-        }
-    }
-]
 export default function History() {
     const [histories, setHistories] = useState<HistoryEntityData[]>([]);
 
@@ -37,12 +13,10 @@ export default function History() {
         (async function () {
             try {
                 // const histories = await PlatformAPI.getMyHistories();
-                const histories = await PlatformAPI.getHistories();
+                const histories = await PlatformAPI.getMyHistories();
                 if (!completed) {
-                    console.log('history');
+                    setHistories(histories);
                     console.log(histories);
-                    // @ts-ignore
-                    setHistories(Dummy);
                 }
             } catch (error) {
                 console.error(error);
@@ -57,10 +31,10 @@ export default function History() {
 
     return (
         <div className="contents">
-            <div className="w-full m-2 md:m-10 mt-24">
+            <div className="w-full m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
                 <div className="flex justify-between items-center">
                     <div className="flex">
-                        <Header title="Histories"/>
+                        <Header category="" title="Histories"/>
                     </div>
                 </div>
                 <div className="mt-10 max-h-screen overflow-auto">
--- src/pages/history/HistoryList.tsx
+++ src/pages/history/HistoryList.tsx
@@ -1,22 +1,20 @@
 import {Badge, Table} from "flowbite-react";
+import {historyColumn} from "../../assets/Dummy";
 import React from "react";
-import {HistoryEntityData} from "../../types/chameleon-platform.common";
+import {HistoryEntityData, HistoryStatus} from "../../types/chameleon-platform.common";
+import {DateUtils} from "../../utils/DateUtils";
 
 type HistoryRow = {
     rows: HistoryEntityData[]
 }
 
-const historyColumn = {
-    list: ['index', 'model name', 'started time', 'ended time', 'executor', 'status']
-}
-
-function getStatusColor(status: string) {
-    if (status === 'cached') return 'gray';
-    if (status === 'initializing') return 'info';
-    if (status === 'running') return 'success';
-    if (status === 'error') return 'failure';
-    if (status === 'finished') return 'indigo';
-    else return 'pink';
+function getStatusColor(status: HistoryStatus):string{
+    if (status == 'cached') return 'gray'
+    if (status == 'initializing') return 'info'
+    if (status == 'running') return 'success'
+    if (status == 'error') return 'failure'
+    if (status == 'finished') return 'indigo'
+    else return 'pink'
 }
 
 export function HistoryList({rows}: HistoryRow) {
@@ -29,18 +27,24 @@ export function HistoryList({rows}: HistoryRow) {
                     ))}
                 </Table.Head>
                 <Table.Body className="divide-y">
-                    {rows.map((row) => (
-                        <Table.Row key={`history-${row.id}`} className={'bg-white'}>
-                            <Table.Cell>{row.id}</Table.Cell>
-                            <Table.Cell>{'name'}</Table.Cell>
-                            <Table.Cell>{'data'}</Table.Cell>
-                            <Table.Cell>{'date'}</Table.Cell>
-                            <Table.Cell>{'executor'}</Table.Cell>
-                            <Table.Cell>
-                                <div className="flex"><Badge color="purple">{row.status}</Badge></div>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
+                    {
+                        rows.length!=0?rows.map((row) => (
+                            <Table.Row key={`history-${row.id}`} className={'bg-white'}>
+                                <Table.Cell>{row.id}</Table.Cell>
+                                <Table.Cell>{row.model.name?row.model.name:''}</Table.Cell>
+                                <Table.Cell>{DateUtils.formatDate(row.startedTime)}</Table.Cell>
+                                <Table.Cell>{DateUtils.formatDate(row.endedTime)}</Table.Cell>
+                                <Table.Cell>{row.executor.username}</Table.Cell>
+                                <Table.Cell>
+                                    <div className="flex"><Badge color={getStatusColor(row.status)}>{row.status}</Badge></div>
+                                </Table.Cell>
+                            </Table.Row>
+                        )): <>
+                            <Table.Row>
+                                <Table.Cell className={'text-center'} colSpan={6}>History Data can not found</Table.Cell>
+                            </Table.Row>
+                        </>
+                    }
                 </Table.Body>
             </Table>
         </div>
