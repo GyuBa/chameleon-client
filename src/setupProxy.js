a489875801a0285e466f6801079d51a20261a4ee
refracta <58779799+refracta@users.noreply.github.com>
2023-05-17 11:21:46 +0900

    Add uploads path proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,23 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
-const backendPaths = ['/api'];
 const useLocal = process.env.USE_LOCAL === 'true' || false;
 console.log('setupProxy.js useLocal:', useLocal);
 
 module.exports = function (app) {
-    backendPaths.forEach(path => {
-        app.use(
-            createProxyMiddleware(path, {
-                target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            })
-        )
-    });
+    app.use(
+        createProxyMiddleware('/api', {
+            target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
+        })
+    );
+    app.use(
+        createProxyMiddleware('/uploads', {
+            target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
+            changeOrigin: true,
+        })
+    );
     app.use(
         createProxyMiddleware('/websocket', {
             target: useLocal ? 'http://localhost:5000' : 'https://dev-platform.chameleon.best',
