901a566e42f5404b3e505a6e79b5e16700890eb8
refracta <58779799+refracta@users.noreply.github.com>
2023-05-19 01:34:46 +0900

    Minor fix

--- src/pages/history/list/HistoryListTable.tsx
+++ src/pages/history/list/HistoryListTable.tsx
@@ -1,5 +1,4 @@
 import {Badge, Table} from "flowbite-react";
-import {historyColumn} from "../../../assets/Dummy";
 import React from "react";
 import {HistoryEntityData, HistoryStatus} from "../../../types/chameleon-platform.common";
 import {DateUtils} from "../../../utils/DateUtils";
@@ -9,47 +8,57 @@ type HistoryRow = {
     rows: HistoryEntityData[]
 }
 
-function getStatusColor(status: HistoryStatus):string{
-    if (status == 'cached') return 'gray'
-    if (status == 'initializing') return 'info'
-    if (status == 'running') return 'success'
-    if (status == 'error') return 'failure'
-    if (status == 'finished') return 'indigo'
-    else return 'pink'
+function getStatusColor(status: HistoryStatus): string {
+    switch (status) {
+        case HistoryStatus.CACHED:
+            return 'gray'
+        case HistoryStatus.INITIALIZING:
+            return 'info'
+        case HistoryStatus.RUNNING:
+            return 'success'
+        case HistoryStatus.ERROR:
+            return 'failure'
+        case HistoryStatus.FINISHED:
+            return 'indigo'
+    }
 }
+
+const historyColumn = ['index', 'model name', 'stated time', 'ended time', 'excutor', 'status'];
 
 export function HistoryListTable({rows}: HistoryRow) {
     const navigate = useNavigate();
     const navigateToDetail = (id: number) => {
-        navigate('/history/detail',{state: rows[id]})
+        navigate('/history/detail', {state: rows[id]})
     }
     return (
         <div>
             <Table hoverable={true}>
                 <Table.Head>
-                    {historyColumn.list.map((item, index) => (
+                    {historyColumn.map((item, index) => (
                         <Table.HeadCell key={index}>{item}</Table.HeadCell>
                     ))}
                 </Table.Head>
                 <Table.Body className="bg-white">
                     {
-                        rows.length!=0?rows.map((row, index) => (
+                        rows.length != 0 ? rows.map((row, index) => (
                             <Table.Row key={row.id} className={'bg-white'} onClick={() => navigateToDetail(index)}>
                                 <Table.Cell>{row.id}</Table.Cell>
-                                <Table.Cell>{row.model?(
-                                    row.model.name ) : (
-                                        <p style={{color: "red"}}>deleted</p>
+                                <Table.Cell>{row.model ? (
+                                    row.model.name) : (
+                                    <p style={{color: "red"}}>deleted</p>
                                 )}</Table.Cell>
                                 <Table.Cell>{DateUtils.formatDate(row.startedTime)}</Table.Cell>
-                                <Table.Cell>{row.endedTime?DateUtils.formatDate(row.endedTime): '-'}</Table.Cell>
+                                <Table.Cell>{row.endedTime ? DateUtils.formatDate(row.endedTime) : '-'}</Table.Cell>
                                 <Table.Cell>{row.executor.username}</Table.Cell>
                                 <Table.Cell>
-                                    <div className="flex"><Badge color={getStatusColor(row.status)}>{row.status}</Badge></div>
+                                    <div className="flex"><Badge color={getStatusColor(row.status)}>{row.status}</Badge>
+                                    </div>
                                 </Table.Cell>
                             </Table.Row>
-                        )): <>
+                        )) : <>
                             <Table.Row>
-                                <Table.Cell className={'text-center'} colSpan={6}>History Data can not found</Table.Cell>
+                                <Table.Cell className={'text-center'} colSpan={6}>History Data can not
+                                    found</Table.Cell>
                             </Table.Row>
                         </>
                     }
