0263e3d90ef866e4db3362731994ea4da4958e08
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-05-18 21:52:02 +0900

    Add user point UI in Account

--- src/pages/profile/Account.tsx
+++ src/pages/profile/Account.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BsPersonCircle} from "react-icons/bs";
-import {HiOutlineLockClosed} from "react-icons/hi";
+import {HiOutlineLockClosed, HiOutlineMail} from "react-icons/hi";
 import {Link} from "react-router-dom";
 import useGetUserInfo from "../../service/authentication/UserInfoService";
 import Button from "../../components/button/Button";
 import Header from "../../components/layout/Header";
+import {GrMoney} from "react-icons/gr";
 
 export default function Account() {
   const {user} = useGetUserInfo();
@@ -15,11 +16,24 @@ export default function Account() {
         <Header title="Account"/>
         <div className="my-4 border-gray-400 rounded-3xl border-1 p-6">
           <p className="text-xs text-gray-600 mb-1 pb-2">User Info</p>
-          <div className="flex items-center">
-            <BsPersonCircle className="w-20 h-20"/>
-            <div className="w-full p-3">
-              <p className="font-extrabold text-xl">{user.username}</p>
-              <p>{user.email}</p>
+          <div className="flex items-center border-b-1 border-gray-300 mx-4 pb-4 mb-4 gap-5">
+            <BsPersonCircle className="w-10 h-10"/>
+            <p className="text-2xl font-extrabold">{user.username}</p>
+          </div>
+          <div className="mx-4 text-16">
+            <div className="flex m-2 gap-4">
+              <div className="flex gap-2 items-center">
+                <HiOutlineMail/>
+                <p className="font-semibold w-16">Email</p>
+              </div>
+              <p className="text-gray-600 font-semibold">{user.email}</p>
+            </div>
+            <div className="flex m-2 gap-4">
+              <div className="flex gap-2 items-center">
+                <GrMoney/>
+                <p className="font-semibold w-16">Point</p>
+              </div>
+              <p className="text-gray-600 font-semibold">₩{user.point.toLocaleString('ko-KR')}</p>
             </div>
           </div>
         </div>
@@ -27,7 +41,7 @@ export default function Account() {
           <p className="text-xs text-gray-600 mb-1 pb-2">Security Setting</p>
           <div className="flex items-center">
             <HiOutlineLockClosed className="mx-4 w-10 h-10"/>
-            <p className="w-full p-2">Password</p>
+            <p className="w-full p-2 font-semibold">Password</p>
             <Link to="/change-password">
               <Button className="color-btn text-sm p-2" text="change"/>
             </Link>
