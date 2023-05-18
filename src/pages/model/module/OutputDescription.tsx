0cd831f27f89e1a074d77ea93fc0c82b06111b0e
refracta <58779799+refracta@users.noreply.github.com>
2023-05-19 01:35:38 +0900

    Minor fix

--- src/pages/model/module/OutputDescription.tsx
+++ src/pages/model/module/OutputDescription.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {HistoryEntityData, HistoryStatus} from "../../../types/chameleon-platform.common";
 
-export default function OutputDescriptionModule(executeData : HistoryEntityData) {
+export default function OutputDescriptionModule(executeData: HistoryEntityData) {
 
     const modelCreatedTime = new Date(executeData?.createdTime)
     const modelEndedTime = new Date(executeData?.endedTime)
@@ -12,7 +12,8 @@ export default function OutputDescriptionModule(executeData : HistoryEntityData)
 
     return (
         <div className="row-span-1 col-span-2 rounded-lg border-1 border-gray-300 overflow-auto">
-            <div className="md:p-2 space-x-3 flex justify-between items-center border-b border-gray-300" style={{ backgroundColor: '#F6F6F6' }}>
+            <div className="md:p-2 space-x-3 flex justify-between items-center border-b border-gray-300"
+                 style={{backgroundColor: '#F6F6F6'}}>
                 <p className="text-xl font-semibold">Output Description</p>
             </div>
             <div className="overflow-y-auto max-h-[80px]">
