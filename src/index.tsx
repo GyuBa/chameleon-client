d41ea5607dc8df46e452db976c223e5193a82cf0
refracta <58779799+refracta@users.noreply.github.com>
2023-05-19 01:35:00 +0900

    Add main.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/main.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ContextProvider} from './contexts/ContextProvider';
