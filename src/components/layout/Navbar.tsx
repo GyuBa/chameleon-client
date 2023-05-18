563d5f3301d0c94f60d504fc7f91d47c57c4814e
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-05-18 21:11:30 +0900

    Add user point UI in Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,24 +5,30 @@ import {MdKeyboardArrowDown} from 'react-icons/md';
 import {useStateContext} from '../../contexts/ContextProvider';
 import useGetUserInfo from "../../service/authentication/UserInfoService";
 import UserProfile from "./UserProfile";
+import {GrMoney} from "react-icons/gr";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
     const {handleClick, isClicked, handleActiveMenu} = useStateContext();
     const {user} = useGetUserInfo();
 
     return (
-        <div className="flex w-full justify-between p-2 relative z-40 bg-white">
+        <div className="flex w-full justify-between p-1 relative z-50 bg-white">
             <button type="button" onClick={handleActiveMenu}
                     className="main-color relative text-xl rounded-full p-3 hover:bg-light-gray"
             ><AiOutlineMenu/></button>
             <div className="flex">
-                <div onClick={() => handleClick('userProfile')}
-                     className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg">
-                    <BsFillPersonFill className="w-5 h-5"/>
+                <Link to='/payment' className="flex items-center gap-2 cursor-pointer px-2 mx-2 hover:bg-light-gray rounded-lg">
+                    <GrMoney className="w-5 h-5"/>
                     <p>
-                        <span className="text-gray-700 text-14">Welcome,</span>{' '}
-                        <span className="text-gray-700 font-bold ml-1 text-14">{user.username}</span>
+                        <span className="text-gray-700 text-14">My point </span>
+                        <span className="text-gray-700 font-bold ml-1 text-14">{user.point}</span>
                     </p>
+                </Link>
+                <div onClick={() => handleClick('userProfile')}
+                     className="flex items-center gap-2 cursor-pointer px-2 hover:bg-light-gray rounded-lg">
+                    <BsFillPersonFill className="w-5 h-5"/>
+                    <span className="text-gray-700 font-bold ml-1 text-14">{user.username}</span>
                     <MdKeyboardArrowDown className="text-gray-400 text-14"/>
                 </div>
                 {isClicked.userProfile && (<UserProfile/>)}
