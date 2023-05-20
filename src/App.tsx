db3ac5596a80430643a4a91e7ec06d927afb9256
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2023-05-21 00:08:24 +0900

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -35,13 +35,13 @@ export default function App() {
     useEffect(() => {
         let message = lastJsonMessage as any;
         if (lastJsonMessage && message.msg === WSMessageType.READY) {
-            sendJsonMessage({msg: WSMessageType.PATH, path: path});
+            sendJsonMessage({msg: WSMessageType.PATH, path});
         }
-    }, [sendJsonMessage, lastJsonMessage, path]);
+    }, [lastJsonMessage, path]);
 
     useEffect(() => {
-        sendJsonMessage({msg: WSMessageType.PATH, path: path});
-    }, [sendJsonMessage, path]);
+        sendJsonMessage({msg: WSMessageType.PATH, path});
+    }, [path]);
 
     return (
         <Routes>
