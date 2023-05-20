8d4ec687a347494bc4baca8ae7d110ee8342edd5
refracta <58779799+refracta@users.noreply.github.com>
2023-05-21 06:01:37 +0900

    Minor fix

---
+++ src/pages/model/module/core/InputModule.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SingleInputModule from "../input/SingleInputModule"
+import EmptyInputModule from "../input/EmptyInputModule"
+import {HistoryEntityData, ModelInputType, ModelParameters} from "../../../../types/chameleon-platform.common";
+import {InputModelInfo} from "../../../../types/chameleon-client";
+import {PageType} from "../../../../types/chameleon-client.enum";
+
+export default function InputModule(type: PageType, parameters: ModelParameters, modelData: InputModelInfo, executeData: HistoryEntityData) {
+    let inputType = modelData?.inputType;
+    let Module;
+
+    if (inputType === ModelInputType.EMPTY) {
+        Module = (parameters && modelData) ? () => EmptyInputModule(type, parameters, modelData, executeData) : null;
+    } else if (inputType === undefined) {
+        Module = null;
+    } else
+        Module = (parameters && modelData) ? () => SingleInputModule(type, parameters, modelData, executeData) : null;
+
+    return (
+        <div className="row-span-2 rounded-lg border-1 border-gray-300 overflow-auto">
+            {Module ? <Module/> : <></>}
+        </div>
+    );
+}
