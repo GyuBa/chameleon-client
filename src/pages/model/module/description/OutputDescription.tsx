9b89e77c1e277b94329bd59045f102d605361cf9
refracta <58779799+refracta@users.noreply.github.com>
2023-05-21 06:03:07 +0900

    Remove Date constructor wrapping

--- src/pages/model/module/description/OutputDescription.tsx
+++ src/pages/model/module/description/OutputDescription.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import {HistoryEntityData, HistoryStatus} from "../../../types/chameleon-platform.common";
+import {HistoryEntityData, HistoryStatus} from "../../../../types/chameleon-platform.common";
 
 export default function OutputDescriptionModule(executeData: HistoryEntityData) {
-
-    const modelCreatedTime = new Date(executeData?.createdTime)
-    const modelEndedTime = new Date(executeData?.endedTime)
-    const status = executeData?.status
-
+    const modelCreatedTime = executeData?.createdTime;
+    const modelEndedTime = executeData?.endedTime;
+    const status = executeData?.status;
     const modelExecuteTime = ((modelEndedTime.getTime() - modelCreatedTime.getTime()) / 3600000).toFixed(6);
-
-
     return (
         <div className="row-span-1 col-span-2 rounded-lg border-1 border-gray-300 overflow-auto">
             <div className="md:p-2 space-x-3 flex justify-between items-center border-b border-gray-300"
