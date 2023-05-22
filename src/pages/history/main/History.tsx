c4c7893e303a1e8022812eb7e1fbd3fa6e64ba0a
Gyuba <sansam1010@naver.com>
2023-05-22 18:39:23 +0900

    fix: History Detail view

--- src/pages/history/main/History.tsx
+++ src/pages/history/main/History.tsx
@@ -1,30 +1,53 @@
-import React, {useState} from 'react';
-import {Link, useLocation} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Link, useParams} from "react-router-dom";
 import OutputDescriptionModule from "../../model/module/description/OutputDescriptionModule";
 import {JsonForms} from "@jsonforms/react";
 import {materialCells, materialRenderers} from "@jsonforms/material-renderers";
 import {JsonViewer} from "@textea/json-viewer";
 import {InputModelInfo, ModuleData} from "../../../types/chameleon-client";
 import {PageType} from "../../../types/chameleon-client.enum";
-import {SitePaths} from "../../../types/chameleon-platform.common";
+import {HistoryEntityData, SitePaths} from "../../../types/chameleon-platform.common";
 import InputModule from "../../model/module/core/InputModule";
 import OutputModule from "../../model/module/core/OutputModule";
+import {PlatformAPI} from "../../../platform/PlatformAPI";
+import {Oval} from "react-loader-spinner";
 
 export default function History() {
-    const location = useLocation();
-    const historyData = location.state;
-    const parameter = historyData.parameters.parameter;
-
+    const {historyId} = useParams();
     const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [historyData, setHistoryData] = useState<HistoryEntityData>();
+
+    useEffect(() => {
+        (async function () {
+            try {
+                const history = await PlatformAPI.getHistory(Number(historyId) - 1);
+                console.log(history);
+                setHistoryData(history);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
+
+    if (historyData === undefined) {
+        return (
+            <div className="contents">
+                <div className="flex justify-center items-center h-screen">
+                    <div className="text-center">
+                        <Oval color="#00BFFF" height={80} width={80}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     const inputModelData: InputModelInfo = {id: -1, inputType: historyData.inputType};
 
     const moduleData: ModuleData = {
         history: historyData!,
         model: inputModelData!,
         type: PageType.EXECUTE,
-        parameters : parameter
+        parameters : historyData.parameters
     };
-
     return (
         <div className="contents">
             <div className="w-full m-2 md:m-10 mt-24">
@@ -46,7 +69,7 @@ export default function History() {
                     <div className="row-span-2 rounded-lg border-1 border-gray-300 overflow-auto">
                         <div className="border-b border-gray-300" style={{backgroundColor: '#F6F6F6'}}>
                             <div className="flex md:p-2 space-x-3 rounded-lg">
-                                // TODO: History Model 페이지 중복 코드 제거
+                                {/*// TODO: History Model 페이지 중복 코드 제거*/}
                                 {['Parameters', 'Parameters (JSON)'].map((label, idx) => {
                                     return (
                                         <button
@@ -66,14 +89,14 @@ export default function History() {
                         <div className="tab-content tab-space overflow-y-auto max-h-[212px] md:p-2">
                             <div className={activeTabIndex === 0 ? "block" : "hidden"}>
                                 <JsonForms
-                                    data={parameter}
+                                    data={historyData.parameters}
                                     renderers={materialRenderers}
                                     cells={materialCells}
                                     readonly
                                 />
                             </div>
                             <div className={activeTabIndex === 1 ? "block" : "hidden"}>
-                                <JsonViewer value={parameter ? parameter : {}}/>
+                                <JsonViewer value={historyData?.parameters ? historyData.parameters : {}}/>
                             </div>
                         </div>
                     </div>
