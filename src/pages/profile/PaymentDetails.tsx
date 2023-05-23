8d55dcde2581f412c0a74f80cb89928bc270652e
SOO YEON CHOI <87463004+sooyeoniya@users.noreply.github.com>
2023-05-23 13:40:34 +0900

    Add getPointsHistories APi

--- src/pages/profile/PaymentDetails.tsx
+++ src/pages/profile/PaymentDetails.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MdPayment} from "react-icons/md";
 import {Link} from "react-router-dom";
-import {SitePaths} from "../../types/chameleon-platform.common";
+import {PointHistoryEntityData, SitePaths} from "../../types/chameleon-platform.common";
 import {FaRegPauseCircle} from "react-icons/fa";
+import {PlatformAPI} from "../../platform/PlatformAPI";
 
 export default function PaymentDetails() {
+	const [pointHistoryData, setPointHistoryData] = useState<PointHistoryEntityData[]>();
+
+	useEffect(() => {
+		(async function () {
+			try {
+				const result = await PlatformAPI.getPointsHistories();
+				setPointHistoryData(result);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
+
+	console.log(pointHistoryData);
 	return (
 		<div className="contents">
-			<div className="w-2/3 m-2 md:m-10 mt-24 md:p-10">
+			<div className="w-2/3 m-2 md:m-10 mt-24 md:px-10 md:pt-10">
 				<p className='head-text'>Payment Details</p>
 				<div className="my-4 border-gray-400 rounded-3xl border-1 p-6">
 					<div className="flex justify-between mb-2">
@@ -16,26 +31,28 @@ export default function PaymentDetails() {
 							<button className="blue-btn text-sm p-2">back</button>
 						</Link>
 					</div>
-					<div className="flex items-center">
-						<FaRegPauseCircle className="mx-2 w-10 h-10"/>
-						<div className="w-full pl-2">
-							<div className="font-semibold text-left">Image output model</div>
-							<div className="text-xs text-gray-600 text-left">2023.05.22 08:09:21</div>
+					<div className="px-3 h-[480px] overflow-auto">
+						<div className="flex items-center">
+							<FaRegPauseCircle className="mx-2 w-10 h-10"/>
+							<div className="w-full pl-2">
+								<div className="font-semibold text-left">Image output model</div>
+								<div className="text-xs text-gray-600 text-left">2023.05.22 08:09:21</div>
+							</div>
+							<div className="my-2">
+								<div className="font-semibold text-red-500 text-right">-500</div>
+								<div className="text-xs text-gray-600 text-right">116,446</div>
+							</div>
 						</div>
-						<div className="my-2">
-							<div className="font-semibold text-red-500 text-right">-500</div>
-							<div className="text-xs text-gray-600 text-right">116,446</div>
-						</div>
-					</div>
-					<div className="flex items-center">
-						<MdPayment className="mx-2 w-10 h-10"/>
-						<div className="w-full pl-2">
-							<div className="font-semibold text-left">Charge Points</div>
-							<div className="text-xs text-gray-600 text-left">2023.05.22 08:09:21</div>
-						</div>
-						<div className="my-2">
-							<div className="font-semibold text-green-500 text-right">+10000</div>
-							<div className="text-xs text-gray-600 text-right">116,946</div>
+						<div className="flex items-center">
+							<MdPayment className="mx-2 w-10 h-10"/>
+							<div className="w-full pl-2">
+								<div className="font-semibold text-left">Charge Points</div>
+								<div className="text-xs text-gray-600 text-left">2023.05.22 08:09:21</div>
+							</div>
+							<div className="my-2">
+								<div className="font-semibold text-green-500 text-right">+10000</div>
+								<div className="text-xs text-gray-600 text-right">116,946</div>
+							</div>
 						</div>
 					</div>
 				</div>
