1ffd2385ab0067832a1dc5e7c9da6fef5248386f
Gyuba <sansam1010@naver.com>
2023-05-24 02:55:16 +0900

    feat: add delete button

--- src/pages/model/board/panel/ModelsDescriptionPanel.tsx
+++ src/pages/model/board/panel/ModelsDescriptionPanel.tsx
@@ -7,7 +7,7 @@ import {ModelEntityData, SitePaths} from "../../../../types/chameleon-platform.c
 import {DescriptionProps} from "../../../../types/chameleon-client";
 import {MdOutlineCancel} from "react-icons/md";
 
-export default function ModelsDescriptionPanel({modelId, setSelectedModelId}: DescriptionProps) {
+export default function ModelsDescriptionPanel({modelId, setSelectedModelId, setModalOpen}: DescriptionProps) {
     const [modelData, setModelData] = useState<ModelEntityData>();
     const navigate = useNavigate();
 
@@ -44,7 +44,7 @@ export default function ModelsDescriptionPanel({modelId, setSelectedModelId}: De
                     className="flex justify-between items-center pb-6 border-b-1 border-gray-300 overflow-auto max-h-screen">
                     <p className="text-3xl font-extrabold tracking-tight text-slate-900">{modelData?.name}</p>
                     <div className="flex gap-2 items-center">
-                        <button className="submit-btn text-sm w-full p-1.5" onClick={handleStart}>start</button>
+                        <button className="submit-btn text-sm w-full p-1.5" onClick={() => setModalOpen(true)}>start</button>
                         <button className="submit-btn text-sm w-full p-1.5" onClick={handleStart}>delete</button>
                         <button className="text-gray-500 text-2xl rounded-full hover:text-black hover:bg-light-gray"
                                 onClick={() => setSelectedModelId(-1)}><MdOutlineCancel/></button>
