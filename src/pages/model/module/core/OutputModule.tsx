9d8d243e897f0c34ae101ab28e3749dcd950221e
refracta <58779799+refracta@users.noreply.github.com>
2023-05-26 11:17:55 +0900

    Minor fix

--- src/pages/model/module/core/OutputModule.tsx
+++ src/pages/model/module/core/OutputModule.tsx
@@ -4,9 +4,7 @@ import {OutputModuleMap} from "../../../../types/chameleon-client.enum";
 import EmptyOutputModule from "../output/EmptyOutputModule";
 
 export default function OutputModule(executeData: HistoryEntityData) {
-
     const Module = executeData?.outputType ? OutputModuleMap[executeData.outputType] : EmptyOutputModule
-
     return (
         <div className="row-span-3 col-span-2 rounded-lg border-1 border-gray-300 overflow-auto">
             <Module {...executeData}/>
