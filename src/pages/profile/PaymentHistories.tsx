b219b5f34652a985db8fe9bf9e07bb2b25c29b9f
sooyeoniya <tndus1306403@naver.com>
2023-05-28 16:44:11 +0900

    Fixed payment histories view error

--- src/pages/profile/PaymentHistories.tsx
+++ src/pages/profile/PaymentHistories.tsx
@@ -58,65 +58,136 @@ export default function PaymentHistories() {
 						>Revenue Histories
 						</button>
 					</div>
-					<div className={pointHistoriesData?.length ? `pl-3 pr-2 contents-area-payment flex flex-col` : `pl-3 pr-2 flex flex-col`}>
-						<div className="overflow-auto contents-area-payment-scroll">
-							{activeTab === PaymentHistoriesType.USAGE && (
-								pointHistoriesData?.length ? (
-									pointHistoriesData?.slice(0).reverse().map((index) => (
-										<div className="flex items-center">
-											{index.type === PointHistoryType.USE_PAID_MODEL
-												? <HiChip className="w-10 h-10"/> : <MdPayment className="w-10 h-10"/>}
-											<div className="w-full pl-2">
-												<div className="font-semibold text-left">
-													{index.modelHistory?.model?.name == null ? (
-														index.type === PointHistoryType.USE_PAID_MODEL ? 'Deleted Model' : 'Charge Points'
-													) : index.modelHistory?.model?.name}
-												</div>
-												<div className="text-xs text-gray-600 text-left">{TimeUtils.formatTime(new Date(index.createdTime))}</div>
-											</div>
-											<div className="my-2 mr-2">
-												<div className={index.type === PointHistoryType.USE_PAID_MODEL
-													? `font-semibold text-red-500 text-right`
-													: `font-semibold text-green-500 text-right`}>{index.delta.toLocaleString('ko-KR')}</div>
-												<div className="text-xs text-gray-600 text-right">{index.leftPoint.toLocaleString('ko-KR')}</div>
-											</div>
-										</div>
-									))) : (
-									<p className="mt-2 text-center text-gray-700">No usage histories found.</p>
-								)
-							)}
-							{activeTab === PaymentHistoriesType.REVENUE && (
-								earnedPointHistoriesData?.length ? (
-									earnedPointHistoriesData?.slice(0).reverse().map((index) => (
-										<div className="flex items-center">
-											<HiChip className="w-10 h-10"/>
-											<div className="w-full pl-2">
-												<div className="font-semibold text-left">
-													{index.model?.name == null ? 'Deleted Model' : index.model?.name}
-												</div>
-												<div className="flex gap-2">
-													<div className="text-xs text-gray-600 text-left border-r-1 border-gray-300 pr-2">{index.executor?.username}</div>
+					{/*<div className={pointHistoriesData && pointHistoriesData?.length > 10*/}
+					{/*	? `pl-3 pr-2 contents-area-payment flex flex-col` : `pl-3 pr-2 flex flex-col`}>*/}
+					{/*	<div className="overflow-auto contents-area-payment-scroll">*/}
+					{/*		{activeTab === PaymentHistoriesType.USAGE && (*/}
+					{/*			pointHistoriesData?.length ? (*/}
+					{/*				pointHistoriesData?.slice(0).reverse().map((index) => (*/}
+					{/*					<div className="flex items-center">*/}
+					{/*						{index.type === PointHistoryType.USE_PAID_MODEL*/}
+					{/*							? <HiChip className="w-10 h-10"/> : <MdPayment className="w-10 h-10"/>}*/}
+					{/*						<div className="w-full pl-2">*/}
+					{/*							<div className="font-semibold text-left">*/}
+					{/*								{index.modelHistory?.model?.name == null ? (*/}
+					{/*									index.type === PointHistoryType.USE_PAID_MODEL ? 'Deleted Model' : 'Charge Points'*/}
+					{/*								) : index.modelHistory?.model?.name}*/}
+					{/*							</div>*/}
+					{/*							<div className="text-xs text-gray-600 text-left">{TimeUtils.formatTime(new Date(index.createdTime))}</div>*/}
+					{/*						</div>*/}
+					{/*						<div className="my-2 mr-2">*/}
+					{/*							<div className={index.type === PointHistoryType.USE_PAID_MODEL*/}
+					{/*								? `font-semibold text-red-500 text-right`*/}
+					{/*								: `font-semibold text-green-500 text-right`}>{index.delta.toLocaleString('ko-KR')}</div>*/}
+					{/*							<div className="text-xs text-gray-600 text-right">{index.leftPoint.toLocaleString('ko-KR')}</div>*/}
+					{/*						</div>*/}
+					{/*					</div>*/}
+					{/*				))) : (*/}
+					{/*				<p className="mt-2 text-center text-gray-700">No usage histories found.</p>*/}
+					{/*			)*/}
+					{/*		)}*/}
+					{/*		{activeTab === PaymentHistoriesType.REVENUE && (*/}
+					{/*			earnedPointHistoriesData?.length ? (*/}
+					{/*				earnedPointHistoriesData?.slice(0).reverse().map((index) => (*/}
+					{/*					<div className="flex items-center">*/}
+					{/*						<HiChip className="w-10 h-10"/>*/}
+					{/*						<div className="w-full pl-2">*/}
+					{/*							<div className="font-semibold text-left">*/}
+					{/*								{index.model?.name == null ? 'Deleted Model' : index.model?.name}*/}
+					{/*							</div>*/}
+					{/*							<div className="flex gap-2">*/}
+					{/*								<div className="text-xs text-gray-600 text-left border-r-1 border-gray-300 pr-2">{index.executor?.username}</div>*/}
+					{/*								<div className="text-xs text-gray-600 text-left">{TimeUtils.formatTime(new Date(index.createdTime))}</div>*/}
+					{/*							</div>*/}
+					{/*						</div>*/}
+					{/*						<div className="my-2 mr-2">*/}
+					{/*							<div className="font-semibold text-green-500 text-right">{index.delta.toLocaleString('ko-KR')}</div>*/}
+					{/*							<div className="text-xs text-gray-600 text-right">{index.leftEarnedPoint.toLocaleString('ko-KR')}</div>*/}
+					{/*						</div>*/}
+					{/*					</div>*/}
+					{/*				))*/}
+					{/*			) : (*/}
+					{/*				<p className="mt-2 text-center text-gray-700">No revenue histories found.</p>*/}
+					{/*			)*/}
+					{/*		)}*/}
+					{/*	</div>*/}
+					{/*	{activeTab === PaymentHistoriesType.REVENUE && (*/}
+					{/*		<div className="flex border-t mx-2 mt-3 pt-3 justify-between items-center">*/}
+					{/*			<div className="font-semibold">Total Revenue:</div>*/}
+					{/*			<div className="font-semibold">￦{user?.earnedPoint.toLocaleString('ko-KR')}</div>*/}
+					{/*		</div>*/}
+					{/*	)}*/}
+					{/*</div>*/}
+
+					{activeTab === PaymentHistoriesType.USAGE && (
+						<div className={pointHistoriesData && pointHistoriesData?.length > 10
+							? `pl-3 pr-2 contents-area-payment flex flex-col` : `pl-3 pr-2 flex flex-col`}>
+							<div className="overflow-auto contents-area-payment-scroll">
+								{activeTab === PaymentHistoriesType.USAGE && (
+									pointHistoriesData?.length ? (
+										pointHistoriesData?.slice(0).reverse().map((index) => (
+											<div className="flex items-center">
+												{index.type === PointHistoryType.USE_PAID_MODEL
+													? <HiChip className="w-10 h-10"/> : <MdPayment className="w-10 h-10"/>}
+												<div className="w-full pl-2">
+													<div className="font-semibold text-left">
+														{index.modelHistory?.model?.name == null ? (
+															index.type === PointHistoryType.USE_PAID_MODEL ? 'Deleted Model' : 'Charge Points'
+														) : index.modelHistory?.model?.name}
+													</div>
 													<div className="text-xs text-gray-600 text-left">{TimeUtils.formatTime(new Date(index.createdTime))}</div>
 												</div>
+												<div className="my-2 mr-2">
+													<div className={index.type === PointHistoryType.USE_PAID_MODEL
+														? `font-semibold text-red-500 text-right`
+														: `font-semibold text-green-500 text-right`}>{index.delta.toLocaleString('ko-KR')}</div>
+													<div className="text-xs text-gray-600 text-right">{index.leftPoint.toLocaleString('ko-KR')}</div>
+												</div>
 											</div>
-											<div className="my-2 mr-2">
-												<div className="font-semibold text-green-500 text-right">{index.delta.toLocaleString('ko-KR')}</div>
-												<div className="text-xs text-gray-600 text-right">{index.leftEarnedPoint.toLocaleString('ko-KR')}</div>
+										))) : (
+										<p className="mt-2 text-center text-gray-700">No usage histories found.</p>
+									)
+								)}
+							</div>
+						</div>
+					)}
+					{activeTab === PaymentHistoriesType.REVENUE && (
+						<div className={earnedPointHistoriesData && earnedPointHistoriesData?.length > 10
+							? `pl-3 pr-2 contents-area-payment flex flex-col` : `pl-3 pr-2 flex flex-col`}>
+							<div className="overflow-auto contents-area-payment-scroll">
+								{activeTab === PaymentHistoriesType.REVENUE && (
+									earnedPointHistoriesData?.length ? (
+										earnedPointHistoriesData?.slice(0).reverse().map((index) => (
+											<div className="flex items-center">
+												<HiChip className="w-10 h-10"/>
+												<div className="w-full pl-2">
+													<div className="font-semibold text-left">
+														{index.model?.name == null ? 'Deleted Model' : index.model?.name}
+													</div>
+													<div className="flex gap-2">
+														<div className="text-xs text-gray-600 text-left border-r-1 border-gray-300 pr-2">{index.executor?.username}</div>
+														<div className="text-xs text-gray-600 text-left">{TimeUtils.formatTime(new Date(index.createdTime))}</div>
+													</div>
+												</div>
+												<div className="my-2 mr-2">
+													<div className="font-semibold text-green-500 text-right">{index.delta.toLocaleString('ko-KR')}</div>
+													<div className="text-xs text-gray-600 text-right">{index.leftEarnedPoint.toLocaleString('ko-KR')}</div>
+												</div>
 											</div>
-										</div>
-									))
-								) : (
-									<p className="mt-2 text-center text-gray-700">No revenue histories found.</p>
-								)
+										))
+									) : (
+										<p className="mt-2 text-center text-gray-700">No revenue histories found.</p>
+									)
+								)}
+							</div>
+							{activeTab === PaymentHistoriesType.REVENUE && (
+								<div className="flex border-t mx-2 mt-3 pt-3 justify-between items-center">
+									<div className="font-semibold">Total Revenue:</div>
+									<div className="font-semibold">￦{user?.earnedPoint.toLocaleString('ko-KR')}</div>
+								</div>
 							)}
 						</div>
-						{activeTab === PaymentHistoriesType.REVENUE && (
-							<div className="flex border-t mx-2 mt-3 pt-3 justify-between items-center contents-area-payment-total">
-								<div className="font-semibold">Total Revenue:</div>
-								<div className="font-semibold">￦{user?.earnedPoint.toLocaleString('ko-KR')}</div>
-							</div>
-						)}
-					</div>
+					)}
 				</div>
 			</div>
 		</div>
