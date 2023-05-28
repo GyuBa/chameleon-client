4735a5c334e7e179ed98ecb23e642de4e66d9386
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2023-05-28 22:36:16 +0900

    Fix ExecuteDescriptionPanel scroll

--- src/pages/model/board/panel/ExecuteDescriptionPanel.tsx
+++ src/pages/model/board/panel/ExecuteDescriptionPanel.tsx
@@ -8,7 +8,7 @@ export default function ExecuteDescriptionPanel({modelDescription}: ExecuteDescr
             <div className="contents">
                 <div className="m-2 md:my-5 mt-24 p-2 md:pr-5 md:py-10">
                     <div className="flex items-center">
-                        <div data-color-mode="light" className="my-4">
+                        <div data-color-mode="light" className="my-4 overflow-auto">
                             <MDEditor.Markdown className="py-2" source={modelDescription}/>
                         </div>
                     </div>
